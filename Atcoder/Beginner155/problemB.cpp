d71931766bbff75c5612ec8e3bb56c0eca67149f
= <=>
2020-02-16 23:18:53 +0700

    beginner contest atcoder 155, problem A B C

---
+++ Atcoder/Beginner155/problemB.cpp
@@ -0,0 +1,18 @@
+//link: https://atcoder.jp/contests/abc155/tasks/abc155_b
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 1;i <= n;i++){
+        int temp;
+        cin >> temp;
+        if(temp % 2 == 0){
+            if(temp % 3 != 0 && temp % 5 != 0){
+                cout << "DENIED";
+                return 0;
+            }
+        }
+    }
+    cout << "APPROVED";
+}
