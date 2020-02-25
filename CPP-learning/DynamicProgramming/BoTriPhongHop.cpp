8f3b1a1cb7f298aae6c088a3c0389c94cb4bed53
= <=>
2020-02-25 18:34:25 +0700

    set up bai toan phong hop

---
+++ CPP-learning/DynamicProgramming/BoTriPhongHop.cpp
@@ -0,0 +1,7 @@
+/*Bài toán Arrange meeting room là dạng bài toán sắp xếp lịch cho một phòng họp theo thời gian
+Ví dụ: phòng 1: Ai = 6 - Bi = 8,
+         phòng 2: Ai = 7 - Bi = 8,
+                       8        9
+                       6        7
+                       8        11
+                       9        11*/
