90c27abe48cc3f94a84436e4d7d54c22c115fada
= <=>
2020-05-02 09:40:26 +0700

    Fix CTDL Heap

--- Lectures-C++/DataStructure/Heap.cpp
+++ Lectures-C++/DataStructure/Heap.cpp
@@ -1,83 +1,67 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-//Heap dạng mảng
-void insertNodeArr(vector<int> &arr, int n, int value)
-{
-    n = n + 1;
-    arr.push_back(value);
-    while (n > 1)
-    {
-        int parent = n / 2;
-        if (arr[parent - 1] < arr.back())
-        {
-            swap(arr[parent - 1], arr.back());
-            n = parent;
-        }
-        else
-            return;
-    }
-}
-
 //hàm format lại cây heap thành max heap, 2 nút con luôn nhỏ hơn nút cha
-void bottomUpMAXHeapify(vector<int> &arr, int node, int n)
-{
+void bottomUpMAXHeapify(vector<int> &arr, int node, int n){
     int parent = node;
     int left_child = 2 * parent + 1;
     int right_child = (2 * parent) + 2;
-    if (left_child <= n)
-    {
-        if (arr[left_child] > arr[parent])
+    if (left_child <= n && right_child <= n){
+        if (arr[left_child] >= arr[right_child])
             parent = left_child;
-    }
-    if (right_child <= n && arr[right_child] > arr[parent])
-    {
-        if (arr[right_child] > arr[parent])
+        else
             parent = right_child;
     }
-    if (parent != node)
-    {
+    else{
+        if (right_child <= n && arr[right_child] > arr[parent]){
+            parent = right_child;
+        }
+        else if (left_child <= n && arr[left_child] > arr[parent]){
+            parent = left_child;
+        }
+    }
+    if (parent != node){
         swap(arr[parent], arr[node]);
         bottomUpMAXHeapify(arr, parent, n);
     }
+    else 
+        return;
 }
 
-
-int main()
-{
+int main(){
     vector<int> arr;
-    vector<int> sorted;
+    vector<int> heap_max;
     int num_element, n_sort;
     cin >> num_element;
     //thêm 1 node vào cây
-    for (int i = 0; i < num_element; i++)
-    {
+    for (int i = 0; i < num_element; i++){
         int value;
         cin >> value;
-        insertNodeArr(arr, i, value);
+        arr.push_back(value);
     }
     //format lại theo dạng max heap
-    for (int i = num_element / 2 - 1; i >= 0; i--)
-    {
+    for (int i = num_element / 2 - 1; i >= 0; i--){
         bottomUpMAXHeapify(arr, i, num_element - 1);
     }
 
-    // n_sort = num_element - 1;
-    // //hàm delete 1 node khỏi cây
-    //xong roi sorted lai
-    // for(int i = num_element - 1;i >= 0; i--){
-    //   swap(arr[i], arr[0]);
-    //   sorted.push_back(arr[i]);
-    //   arr.pop_back();
-    //   bottomUpMAXHeapify(arr, 0, --n_sort);
-    // }
+    n_sort = num_element - 1;
+    for (int i = num_element - 1; i >= 0; i--){
+        //sort lại cây heap
+        swap(arr[i], arr[0]);
+        heap_max.push_back(arr[i]);
+        arr.pop_back();
+        bottomUpMAXHeapify(arr, 0, --n_sort);
+    }
 
-    for (int i = 0; i < num_element; i++)
-    {
-        cout << arr[i] << " ";
+    //cây max heap chưa sort
+    for (int i = 0; i < num_element; i++){
+        cout << heap_max[i] << " ";
     }
     cout << endl;
-    // for(int i = 0;i < num_element;i++){
-    //   cout << sorted[i] << " ";
-    // }
+
+    //cây max heap đã sort lại
+    for (int i = 0; i < num_element; i++){
+        cout << arr[i] << " ";
+    }
 }
