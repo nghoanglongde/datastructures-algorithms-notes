7bab0320d07572e17a0b10457fcef64ceadbb8ec
= <=>
2020-05-06 23:15:24 +0700

    Add Binary Search using Libary Algorithm

--- Lectures-C++/Algorithm/Sort-Search/BinarySearch.cpp
+++ Lectures-C++/Algorithm/Sort-Search/BinarySearch.cpp
@@ -1,25 +1,11 @@
 #include <iostream>
 #include <vector>
+#include<algorithm>
 using namespace std;
 
-int mySqrt(int x) {
-    long long left = 0;
-    long long right = x;
-    long long mid = (left + right) / 2;
-    while (left != mid && right != mid)
-    {
-        if(mid * mid <= x){
-            left = mid;
-        }
-        else
-            right = mid;
-        mid = (left + right) / 2;
-    }
-    return (right * right == x) ? right : left; //trường hợp căn 1
-}
-
+//chặt nhị phân thông thường
 bool BS(vector<int> nums, int target){
-    //Binary Search tìm số trong mảng tăng dần
+    int n = nums.size();
     int left = 0;
     int right =  target;
     int mid = (left + right) / 2;
@@ -32,7 +18,15 @@ bool BS(vector<int> nums, int target){
             right = mid;
         mid = (left + right) / 2;
     }
-    return (nums[left] == target || nums[right] == target) ? true : false;
+    return (nums[mid] == target) ? true : false;
+}
+
+//chặt nhị phân sử dụng thư viện có sẵn
+bool BSLib(vector<int> nums, int target){
+    auto k = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    if (k < nums.size() && nums[k] == target) {
+        return true;
+    }
 }
 
 int main()
@@ -40,24 +34,20 @@ int main()
     //Tìm số trong mảng tăng dần
     vector<int> nums;
     int n, target;
-    cin >> n;
-    cin >> target;
+    cin >> n >> target;
     for (int i = 1; i <= n; i++)
     {
         int temp;
         cin >> temp;
         nums.push_back(temp);
     }
+    
+    BSLib(nums, target);
+
     if(BS(nums, target)){
         cout << "Found" << endl;
     }
     else{
         cout << "Not found" << endl;
     }
-
-    //tìm căn bậc 2 bằng binary search ko sử dụng lib
-    int num_sqrt;
-    cin >> num_sqrt;
-    int result_sqrt = mySqrt(num_sqrt);
-    cout << result_sqrt;
 }
