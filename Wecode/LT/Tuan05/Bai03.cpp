409386785d2316c0506c167c4f5b325ad46c3743
= <=>
2020-05-31 07:54:46 +0700

    Update Binary Search, WeCode - LT Tuan 05 part 1

---
+++ Wecode/LT/Tuan05/Bai03.cpp
@@ -0,0 +1,11 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+class
+template
+[
+]
+###End banned keyword*/
+#include <iostream>
+using namespace std;
+//###INSERT CODE HERE -
