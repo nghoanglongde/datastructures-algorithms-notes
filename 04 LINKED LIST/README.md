668b582bc1729e942c99b4143772cf129e0313b7
nghoanglong <19521788@gm.uit.edu.vn>
2021-07-10 17:31:04 +0700

    palindrome linked list

--- 04 LINKED LIST/README.md
+++ 04 LINKED LIST/README.md
@@ -37,3 +37,5 @@ Code example at: **doubly_linked_list.py**
 ## Practice
 
 [Reverse Linked List](https://user-images.githubusercontent.com/43443323/124485808-25fd5a80-ddd7-11eb-8e3c-63944b633e6c.png)
+
+[Palindrome Linked List](https://user-images.githubusercontent.com/43443323/124485808-25fd5a80-ddd7-11eb-8e3c-63944b633e6c.png)
---
+++ 04 LINKED LIST/palindrome_linked_list.py
@@ -0,0 +1,40 @@
+# Link: https://leetcode.com/problems/palindrome-linked-list/
+# Easy
+# ======================================================
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getHeadOfSecondPart(self, head: ListNode) -> ListNode:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        if fast is None:
+            return slow
+        else:
+            return slow.next
+    def isPalindrome(self, head: ListNode) -> bool:
+        head_second_part = self.getHeadOfSecondPart(head)
+        head_firt_part = head
+
+        # reverse second part
+        prev_node = None
+        cur_node = head_second_part
+        while cur_node is not None:
+            next_node = cur_node.next
+            cur_node.next = prev_node
+            prev_node = cur_node
+            cur_node = next_node
+        head_second_part = prev_node
+
+        
+        while head_second_part is not None:
+            if head_firt_part.val != head_second_part.val:
+                return False
+            head_firt_part = head_firt_part.next
+            head_second_part = head_second_part.next
+        return True 
+        
